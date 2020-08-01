c8c6065ace51aee81d9622463a7405aa886787e0
thesplithorizon <53864589+thesplithorizon@users.noreply.github.com>
2020-08-01 17:29:35 +0800

    Update failover.ps1

--- failover.ps1
+++ failover.ps1
@@ -1,23 +1,27 @@
-#	Powershell PowerNSX script to automate the configuration of 
-#	NSX Edge during failover from Bangi to Bangsar
-#	
-#	
-#       Connect to NSX Manager
-#       Connect-NsxServer -vCenterServer  <vCenter IP/DNS>
+#	    Powershell PowerNSX script to automate the configuration of 
+#	    NSX Edge during failover from Bangi to Bangsar
+#	    
+#	     
+#.    To install PowerNSX,follow the instruction below,
+#.    https://github.com/vmware/powernsx
+#.     
+#.    Before running the script, you need to connect to NSX Manager
+#.    
+#.    Also you need to verify the following variable accordingly, 
+#.    ESG name in $edge 
+#.    Index number in $coreUplink, $transitInternal and  $coreUplinkDr
 #
+#     To connect to NSX Manager, use the following cmdlet
+#     Connect-NsxServer -vCenterServer  <vCenter IP/DNS>
 #
-#
-#
-#
+#.    
 #
 #
 #
 #
 
 
-
-
-#Change the ESG name accordingly 
+#Replace the ESG name accordingly 
 $edges = ("esg-test-1", "esg-test-2")
 
 #For each ESG in the list of Edges 
@@ -31,30 +35,29 @@ Write-Host "Enabling Uplink connect to VLAN 216"
 #Get the Edge and interface you want to modify
 $edge = Get-NsxEdge $esg
 
-#Input the vNIC interface Index number for uplink connect to VLAN 215
-$uplink215 = $edge | Get-NsxEdgeInterface -Index 0
-
-#Input the vNIC interface Index number for uplink connect to VLAN 216
-$uplink216 = $edge | Get-NsxEdgeInterface -Index 3
+#Replace the ESG vNIC interface Index number accordingly
+$coreUplink = $edge | Get-NsxEdgeInterface -Index 0
+$transitInternal = $edge | Get-NsxEdgeInterface -Index 1
+$coreUplinkDr = $edge | Get-NsxEdgeInterface -Index 2
 
 #Get the edgeid child elem that powernsx adds to aid pipeline operations...
-$edgeid1 = $uplink215.SelectSingleNode("child::edgeId")
-$edgeid3 = $uplink216.SelectSingleNode("child::edgeId")
+$edgeid1 = $coreUplink.SelectSingleNode("child::edgeId")
+$edgeid2 = $coreUplinkDr.SelectSingleNode("child::edgeId")
 
 #...and remove it from the xml we send to the api, otherwise it will error.
-[void]$uplink215.RemoveChild($edgeid1)
-[void]$uplink216.RemoveChild($edgeid3)
+[void]$coreUplink.RemoveChild($edgeid1)
+[void]$coreUplinkDr.RemoveChild($edgeid2)
 
-#Connect uplink215 to VLAN 215
-$uplink215.isConnected = "false"
+#Disconnect Core-Uplink from VLAN 216
+$coreUplink.isConnected = "false"
 
-#Disconnect uplink216 from VLAN 216
-$uplink216.isConnected = "true"
+#Connect Core-Uplink-Dr to VLAN 215
+$coreUplinkDr.isConnected = "true"
 
 #Call the API.  Using Invoke-NsxRestMethod means we dont need server name, protocol, port, content-type header or authentication header. 
 #This all comes from the PowerNSX $defaultnsxconnection object created when we run Connect-NsxServer
-Invoke-NsxRestMethod -method put -URI "/api/4.0/edges/$($edge.id)/vnics/$($uplink215.index)" -body $uplink215.OuterXml
-Invoke-NsxRestMethod -method put -URI "/api/4.0/edges/$($edge.id)/vnics/$($uplink216.index)" -body $uplink216.OuterXml
+Invoke-NsxRestMethod -method put -URI "/api/4.0/edges/$($edge.id)/vnics/$($coreUplink.index)" -body $coreUplink.OuterXml
+Invoke-NsxRestMethod -method put -URI "/api/4.0/edges/$($edge.id)/vnics/$($coreUplinkDr.index)" -body $coreUplinkDr.OuterXml
 Write-Host "`nCompleted`n"
 
 
@@ -66,23 +69,22 @@ $edgerouting.ospf.enabled = "false"
 Set-NsxEdgeRouting -EdgeRouting $edgerouting -Confirm:$false
 Write-Host "`nComplete`n"
 
-
 #Change ESG Router ID"
 Write-Host "Changing Router ID"
 
 #Collect the Primary IP address of uplink216
-$uplink216IpAddr = $uplink216.addressGroups.addressGroup.primaryAddress
+$coreUplinkDrIpAddr = $coreUplinkDr.addressGroups.addressGroup.primaryAddress
 
 #Refresh NSX Edge Routing Object 
 $edge = Get-NsxEdge $esg
 $edgerouting = Get-NsxEdgeRouting $edge
 
 #Change Router ID
-$edgerouting.routingGlobalConfig.routerId = $uplink216IpAddr
+$edgerouting.routingGlobalConfig.routerId = $coreUplinkDrIpAddr
 Set-NsxEdgeRouting -EdgeRouting $edgerouting -Confirm:$false
 Write-Host "`nComplete`n"
 
-#Enable OSPF
+#Enable OSPF Back
 Write-Host "Enable OSPF"
 $edge = Get-NsxEdge $esg
 $edgerouting = Get-NsxEdgeRouting $edge
